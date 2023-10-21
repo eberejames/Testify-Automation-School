b6b167cc8d311d46463e413e9b2d9ac11b766b08
eberejames <ebere.arinze@gmail.com>
2023-10-21 03:29:37 +0100

    JS fun facts

---
+++ Moduel 3/Module 3A/Task2.js
@@ -0,0 +1,15 @@
+// FUN FACTS ABOUT JAVASCRIPT
+
+// Despite its name, JavaScript is not related to the Java programming language
+// It was originally called "LiveScript" but was later renamed "JavaScript" to capitalize on the popularity of Java
+// JavaScript took just 10 days to develop
+// JavaScript is the only programming language that web browsers inherently understand which it a vital component for web development
+// JavaScript can also run on servers and databases 
+// JavaScript is dynamically typed
+// JavaScript allows for rapid development due to its interpreted nature and the ability to instantly see changes in a web browser
+// JavaScript has a thriving open-source community, with many libraries and frameworks
+// JavaScript is not limited to web development. It's used in mobile app development,game development and even server-side scripting
+// In JS, you can include emojis in your code and use them as variables, function names, or comments
+// JavaScript continues to evolve and shape the modern web, and its flexibility and ubiquity have made it a fun and powerful language for developers
+// According to GitHub avaScript is the most Popular language in the world
+// JavaScript is popular among developres because its fairly easy to learn
