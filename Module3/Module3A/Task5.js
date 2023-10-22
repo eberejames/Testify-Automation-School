8680252d035a447cd643535dde9049c31e682ffc
eberejames <ebere.arinze@gmail.com>
2023-10-22 18:01:32 +0100

    add files

---
+++ Module3/Module3A/Task5.js
@@ -0,0 +1,13 @@
+
+//var myFavouriteLanguage = "JavaScript"
+
+//console.log("JavaScript")
+
+
+//const currentScore = 30
+
+//console.log(currentScore +30)
+
+const day = 'Wednesday'
+const position = 3
+const output = 'Today is ' + day|+'. '+'it is the '+position+' of the week'
