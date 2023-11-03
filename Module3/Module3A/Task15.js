d231133d2095977faf8aa8db9f44959f35560d4c
eberejames <ebere.arinze@gmail.com>
2023-11-03 02:25:50 +0000

    add files

--- Module3/Module3A/Task15.js
+++ Module3/Module3A/Task15.js
@@ -7,28 +7,28 @@ const books = [
 title: "Love and Basketball",
 description: "Love now wears wheelscreen",
 numberOfPages: 250,
-authour: "Love Doctor Not",
+author: "Love Doctor Not",
 reading: true,
 },
 {
 title: "Love Doctor",
 description: "Love is patient",
 numberOfPages: 252,
-authour: "Love Doctor",
+author: "Love Doctor",
 reading: false,
 },
 {
 title: "Love Love",
 description: "Love is wicked",
 numberOfPages: 345,
-authour: "Love Stranger ",
+author: "Love Stranger ",
 reading: true,
 },
 {
 title: "Love and Hate",
 description: "Love is kind",
 numberOfPages: 359,
-authour: " Doctor kim",
+author: " Doctor kim",
 reading: true,
 },
 
@@ -37,10 +37,10 @@ reading: true,
 for(let numbers = 0; numbers < 5; numbers++)
 {
 if(books[numbers].reading===true){
-console.log("The title of this book is " +books[numbers].title)
-console.log("The book's description is " +books[numbers].description)
-console.log("The book's page number is " +books[numbers].numberOfPages)
-console.log("The book's author is " +books[numbers].authour)
+console.log(books[numbers].title)
+console.log(books[numbers].description)
+console.log(books[numbers].numberOfPages)
+console.log(books[numbers].author)
 console.log("**************************")
 
 }
