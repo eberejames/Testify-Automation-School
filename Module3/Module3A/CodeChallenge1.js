edd679ab87784fd3f93e846d4471223d409e578d
eberejames <ebere.arinze@gmail.com>
2023-11-10 02:36:53 +0000

    add files

--- Module3/Module3A/CodeChallenge1.js
+++ Module3/Module3A/CodeChallenge1.js
@@ -4,7 +4,7 @@
 
 let numbers = [1, 5, 33, 41, 100];                  // create an array
 let sum = 0;                                      // create a variable for the sum and initialize it
-for (let i = 0; i < numberss.length; i++ ) {       // iterate over each item in the array
+for (let i = 0; i < numbers.length; i++ ) {       // iterate over each item in the array
   sum += numbers[i];
 }
 
