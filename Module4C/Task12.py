5e24b708e3e60b0f0e22159e0428a33d680a232b
eberejames <ebere.arinze@gmail.com>
2024-02-03 06:15:06 +0000

    add task12

---
+++ Module4C/Task12.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+# Declare a global variable with name as language and the value as "Python"
+# Create a function, in the function declare a variable with name as language and value as "Java", then print out the variable in the function
+# Print out the variable name into the console
+# Invoke the function
+
+
+
+
+
+# Declare a global variable
+language = "Python"
+
+
+def another_language():
+    language = "Java"
+    print(language)
+
+
+# Invoke the function
+another_language()
