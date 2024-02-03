c0ef4ddcfd1c05d2fb64dd92e8424219ad3745d8
eberejames <ebere.arinze@gmail.com>
2024-02-03 02:00:06 +0000

    add task3

---
+++ Module4C/Task3.py
@@ -0,0 +1,14 @@
+
+# Declare a raw string
+# Declare two strings with any value
+# Concatenate the two string
+# Print out the value of the concatenated strings
+
+
+name = r"I am my Mother\'s first daughter\n the one and only Ada and that\'s on  PERIOD!!!\b"
+print(name)
+
+
+name = "Ebere"
+gender = "girl"
+print(name + " is a " + gender)
