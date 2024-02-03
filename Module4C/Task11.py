8cbdc88d1bc3d088808aabe1d4946cd4937004a2
eberejames <ebere.arinze@gmail.com>
2024-02-03 05:48:55 +0000

    add task11

---
+++ Module4C/Task11.py
@@ -0,0 +1,19 @@
+
+# Create a function that accepts two numbers, adds the numbers and prints out the result in the console.
+# Create a function that return the string value "Testify Python"
+# Invoke/call the two functions
+
+def add(num1, num2):
+    result = num1 + num2
+    print(result)
+
+
+
+def string_value():
+ return  "Testify Python"
+
+
+add(5, 3.5)
+
+value = string_value()
+print(value)
