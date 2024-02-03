bdb6883425e4c1c862926bde833175ad5f161e8b
eberejames <ebere.arinze@gmail.com>
2024-02-03 02:28:19 +0000

    add task4

---
+++ Module4C/Task4.py
@@ -0,0 +1,23 @@
+# Declare two numbers with any values
+# Add or Subtract or Multiply the two numbers together
+# Print out the value of the operation
+
+number = 10
+number += 2
+print(number)
+
+subtract =106 - 6
+print(subtract)
+
+
+multiply = 11 * 11
+print(multiply)
+
+
+
+
+
+
+
+
+
