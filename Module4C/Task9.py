58339cd28d1a8f47fd8f5da751323d1c97000c7b
eberejames <ebere.arinze@gmail.com>
2024-02-03 05:01:20 +0000

    add task9

---
+++ Module4C/Task9.py
@@ -0,0 +1,13 @@
+
+
+
+def greet():                 # Create a function that prints out "Hello World"
+    print("Hello World")
+
+
+def food():                  # Create a function that does nothing, an empty function
+    pass
+
+
+greet()                      # Invoke/call both functions
+food()
