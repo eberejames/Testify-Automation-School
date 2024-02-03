17f6d16fa7f92275555589e5f0267112aeba7648
eberejames <ebere.arinze@gmail.com>
2024-02-03 03:47:12 +0000

    add task6

--- Module4C/Task5.py
+++ Module4C/Task5.py
@@ -2,7 +2,6 @@
 # Check if the variable is equal to 2, if true print out "Number is Two"
 # Add an else block which prints out "Number is not Two"
 
-# If the value of the declared variable is changed from two, it should print "Number is not Two" in the console, if it changed back to 2 it should print "Number is Two" in the console.
 
 number = 2
 if number == 2:
---
+++ Module4C/Task6.py
@@ -0,0 +1,28 @@
+# Declare two numbers with any values
+# Check if the first number is greater than 50 and check if the second number is less than 100, if True print out "Evaluation is True"
+# Add an else block that prints "Evaluation is False"
+
+num1 = 3
+num2 = 5
+
+if num1 > 50 and num2 < 100:
+    print("Evaluation is True")
+
+else:
+    print("Evaluation is False")
+
+
+# Declare a string with value "python"
+# Check if the first string is equal to "testify" or "python", if True prints out "Yay!! The string is a Testify Python".
+
+
+language = "python"
+if language == "testify" or language == "python":
+    print("Yay!! The string is a Testify Python")
+
+
+
+
+
+
+
