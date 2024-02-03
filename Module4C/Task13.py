815d11e9f6396e51b63db3167b22beaa8fc09e1f
eberejames <ebere.arinze@gmail.com>
2024-02-03 10:29:55 +0000

    add Task13

--- Module4C/Task13.py
+++ Module4C/Task13.py
@@ -1,17 +1,10 @@
-Create a function that prints out "Hello World"
-Invoke the same function in it own body
-Invoke the function outside the function block
-
-Take note of the function invoke and put the Python whitespace rule in mind
+# Create a function that prints out "Hello World"
+# Invoke the same function in it own body
+# Invoke the function outside the function block
+# Take note of the function invoke and put the Python whitespace rule in mind
 
 
 
 def word():
     print("Hello World")
 
-    # Invoke the function within its own body
-    print("Invoking the function inside its own body:")
-    word()
-
-# Invoke the function outside the function block
-word()
