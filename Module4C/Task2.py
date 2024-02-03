2ca00b8204e7b78e240d8ff5ae427426491fa76f
eberejames <ebere.arinze@gmail.com>
2024-02-03 00:55:22 +0000

    add task2

---
+++ Module4C/Task2.py
@@ -0,0 +1,13 @@
+# Declare a variable with variable_name as my_var and value as 12.6
+# Declare an integer variable with the value of 200
+# Declare a String variable with the value of your name
+
+
+my_var = 12.6
+print(type(my_var))
+
+my_var = 200
+print(type(my_var))
+
+my_var = "Ebere"
+print(type(my_var))
