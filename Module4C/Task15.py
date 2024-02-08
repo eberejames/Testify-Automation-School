9532bda421feade6d8bc9ed0e7680839df0df96a
eberejames <ebere.arinze@gmail.com>
2024-02-08 20:32:00 +0000

    add task15

---
+++ Module4C/Task15.py
@@ -0,0 +1,18 @@
+
+# Declare a list with any value and number of item/element
+awards = ["Headies", "Oscar", "Grammy\'s", "The noble prize", "AMAA", "Afrima"]
+
+# Print the list in console
+print(awards)
+
+# Sort the list
+awards.sort(reverse=True)
+
+# Print the list in console again
+print(awards)
+
+
+
+
+
+
