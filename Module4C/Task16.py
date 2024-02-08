b2092c12ac4866856ce2a1a25e5f59ab52851203
eberejames <ebere.arinze@gmail.com>
2024-02-08 23:07:52 +0000

    add Task16

---
+++ Module4C/Task16.py
@@ -0,0 +1,15 @@
+#  Declare a dictionary with any key-value pair of items/elements
+family = {
+    "Dad": "Musa",
+    "Mom": "Mary",
+    "Daughters": "Ciara",
+    "Sons": "Anthony"
+}
+# Print the dictionary in console
+print(family)
+
+# Update the dictionary with two different key-value pair items
+family.update({"Uncle": "Moses", "Aunt": "Sarah"})
+
+# Print the dictionary in console again
+print("Updated dictionary:", family)
