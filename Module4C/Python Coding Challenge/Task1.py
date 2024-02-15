5bb6f4f2b3c1202a5c6b53e1576530d1fa1b3fd3
eberejames <ebere.arinze@gmail.com>
2024-02-15 01:57:43 +0000

    add challenge1

---
+++ Module4C/Python Coding Challenge/Task1.py
@@ -0,0 +1,11 @@
+# Calculate the sum of numbers within an array
+
+
+
+num = [24, 15, 30, 14, 91]           # Define an array of numbers
+
+
+sum_of_numbers = sum(num)       # Calculate the sum of numbers within the array
+
+
+print( sum_of_numbers)      # Print the result
